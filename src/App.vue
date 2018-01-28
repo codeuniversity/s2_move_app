71cac5b54d1db041b8d9b249bf815059aa9849d1
Daniele Santos <danielesantos@Danieles-MacBook-Pro.local>
2018-01-28 10:10:04 +0100

    ajax and serach implemented

---
+++ src/App.vue
@@ -0,0 +1,168 @@
+<template>
+  <div id="app">
+    <router-view/>
+    <div class="search-wrapper">
+      <input type="text" v-model="searchTerm" placeholder="Search"/>
+          <label>Search:</label>
+    </div>
+    <div class="wrapper">
+      <div class="card" v-for="post in filteredList">
+        <p> {{ post.fname }} - {{ post.gmailacc }}</p>
+        </a>
+      </div>
+    </div>
+</div>
+</template>
+
+<script>
+
+import { get } from 'axios'
+
+export default {
+  name: 'App',
+   data: function() {
+    return {
+    searchTerm: '',
+    postList: []  
+    }   
+  },
+    methods: {
+
+    fetchPeople(locale) {
+      var self = this;
+      try {
+        get(`/static/data/${locale}.json`)
+            .then(function(response) {
+              self.postList = response.data;
+           })
+            .catch(function(error) {
+              console.error(error);
+            });
+      }
+      catch (error) {
+        console.error(error);
+      }
+    }
+   },  
+  computed: {
+    filteredList() {
+      return this.postList.filter(post => {
+        return post.fname.toLowerCase().includes(this.searchTerm.toLowerCase()) 
+        || post.gmailacc.toLowerCase().includes(this.searchTerm.toLowerCase())
+      })
+    }
+  },
+  beforeMount() {
+    this.fetchPeople("person");
+  }
+}
+
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+#helloimg {
+  width: 200px;
+} 
+html, body {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  margin-top: 16px;
+  margin-bottom: 16px;
+}
+
+div#app {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  
+  .search-wrapper {
+    position: relative;
+    label {
+      position: absolute;
+      font-size: 12px;
+      color: rgba(0,0,0,.50);
+      top: 8px;
+      left: 12px;
+      z-index: -1;
+      transition: .15s all ease-in-out;
+    }
+    input {
+      padding: 4px 12px;
+      color: rgba(0,0,0,.70);
+      border: 1px solid rgba(0,0,0,.12);
+      transition: .15s all ease-in-out;
+      background: white;
+      &:focus {
+        outline: none;
+        transform: scale(1.05);
+        & + label  {
+          font-size: 10px;
+          transform: translateY(-24px) translateX(-12px);
+        }
+      }
+      &::-webkit-input-placeholder {
+          font-size: 12px;
+          color: rgba(0,0,0,.50);
+          font-weight: 100;
+      }
+    }
+  }
+
+  .wrapper {
+    display: flex;
+    max-width: 444px;
+    flex-wrap: wrap;
+    padding-top: 12px;
+  }
+
+  .card {
+    box-shadow: rgba(0, 0, 0, 0.117647) 0px 1px 6px, rgba(0, 0, 0, 0.117647) 0px 1px 4px;
+    max-width: 124px;
+    margin: 12px;
+    transition: .15s all ease-in-out;
+    &:hover {
+      transform: scale(1.1);
+    }
+    a {
+      text-decoration: none;
+      padding: 12px;
+      color: #03A9F4;
+      font-size: 24px;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      img {
+        height: 100px;
+      }
+      small {
+        font-size: 10px;
+        padding: 4px;
+      }
+    }
+  }
+  .hotpink {
+    background: hotpink;
+  }
+
+  .green {
+    background: green;
+  }
+
+  .box {
+    width: 100px;
+    height: 100px;
+    border: 1px solid rgba(0,0,0,.12);
+  }
+}
+</style>
