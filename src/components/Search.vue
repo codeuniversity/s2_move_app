8ca27e6d403f7bc3ee10427489993be75c936340
Daniele Santos <danielesantos@Danieles-MacBook-Pro.local>
2018-02-08 13:16:10 +0100

    made search bar responsive

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,14 +1,12 @@
 <template>
-  <div id="app">
+  <div>
     <div class="search-wrapper">
       <input class="search" type="text" v-model="searchTerm" placeholder="Who are you looking for?"/>
-          <label>TESTING</label>
+          <label></label>
     </div>
     <div class="wrapper" v-if="isListVisible">  
       <div class="card" v-for="person in filteredList">
-        <p @click="showDetails(person)"> {{ person.fName }} {{ person.lName }} - {{ person.gmailAcc }}</p>
-        </a>
-      </div>
+      <p @click="showDetails(person)"> <a href="#">{{ person.fName }} {{ person.lName }} </a> {{ person.gmailAcc }}</p>      </div>
     </div>
     <div v-if="selectedPerson">
       <img :src="selectedPerson.image"> <br>
@@ -76,13 +74,59 @@ export default {
 
 
 <style>
-.search {
-  width: 100%;
+
+@media (max-width: 1280px) {
+  .search {
+  width: 50%;
   height: 35px;
+  float: right;
+  font-size: 16px;
+  display: inline-block;
+  }
 }
+@media (max-width: 1000px) {
+  .search {
+  width: 80%;
+  height: 35px;
+  margin: auto;
+  font-size: 16px;
+  display:inline-block;
+  }
+}
+
+@media (max-width: 600px) {
+  .search {
+  width: 97%;
+  height: 35px;
+  margin-right: 4px;
+  margin-left: 4px;
+  display: inline-block;
+  }
+}
+
+
 label {
   color: white;
   font-size: 40px;
   font-family: Maison;
+}
+.card {
+  color: white;
+}
+
+a {
+  color: white;
+  background-color: rgb(248, 0, 66);
+  text-decoration: none;
+  border: 2px solid white;
+  border-radius: 5px;
+  padding: 10px;
+  margin: 10px;
+  display: inline-block;
+}
+
+
+a:hover {
+  text-decoration: none;
 }
 </style>
