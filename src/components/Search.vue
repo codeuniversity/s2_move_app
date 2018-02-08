83ba3fc49d0f4d14c08251c90bf01f35e6fde11e
Marie Klaus <marie.klaus@code.berlin>
2018-02-08 20:12:46 +0100

    Corrected key from phone to mobile

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -13,7 +13,7 @@
     <div v-if="selectedPerson">
       <img class="cardimg" :src="selectedPerson.image"> <br>
       <p class="textinfocard"> {{ selectedPerson.fName }} {{ selectedPerson.lName }}</p> 
-      <p class="textinfocard">{{ selectedPerson.group }} - Phone: {{ selectedPerson.phone }}</p><br> 
+      <p class="textinfocard">{{ selectedPerson.group }} - Phone: {{ selectedPerson.mobile }}</p><br> 
       <a :href="selectedPerson.googleCal"> Google Calendar</a> 
       <a :href="selectedPerson.wiki"> Wiki</a>
     </div>
