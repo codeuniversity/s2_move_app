6c8316b45315ef8792c1c2542fe48a9d3f8df640
Daniele de Araujo dos Santos <danisantos@Danieles-MacBook-Pro.local>
2018-03-06 20:43:28 +0100

    added routes

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,21 @@
 import VueRouter from "vue-router";
 
+// ## Main Components 
+
+import Auth from "./components/Auth.vue"
+import Header from "./components/Header.vue"
+import Profile from "./components/Profile.vue"
+import Search from "./components/Search.vue"
+
+//## Maps
+
+import Berlin from "./components/maps/Berlin.vue"
+import Frankfurt from "./components/maps/Frankfurt.vue"
+import HamburgOttensen from "./components/maps/HamburgOttensen.vue"
+import Munich from "./components/maps/Munich.vue"
+import Prague from "./components/maps/Prague.vue"
+
+
 
 const router = new VueRouter({
 
