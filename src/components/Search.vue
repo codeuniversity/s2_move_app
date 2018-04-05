45ce79e3baa584424fb12ab1ed32df846e6c5729
Marie Klaus <marie.klaus@code.berlin>
2018-04-05 19:45:33 +0200

    Implemented clear search button in PROFILE.

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,41 +1,41 @@
 <template>
-    <div class="search">
-      
-      <!-- SEARCH INPUT FORM -->
-      <form name="myForm">
-        <input v-on:submit.prevent
-            autocomplete="off"
-            class="search__input"
-            name="mySearch"
-            v-model="searchTerm"
-            type="text" 
-            placeholder="Who are you looking for?"
-            id="clearSearch"
+  <div class="search">
+
+    <!-- SEARCH INPUT FORM -->
+    <form name="myForm">
+      <input 
+        v-on:submit.prevent 
+        autocomplete="off"
+        class="search__input"
+        name="mySearch"
+        v-model="searchTerm"
+        type="text" 
+        placeholder="Who are you looking for?"
+        id="clearSearch"
         />
       </form>
 
     <!-- MENU TOGGLE BUTTON -->
     <div class="btn" @click="toggleMenu()"></div>
 
-      <!-- SEARCH RESULT LIST -->
-      <div class="search__list" v-if="isListVisible">
+    <!-- SEARCH RESULTS -->
+    <div class="search__list" v-if="isListVisible">
       <!-- CLEAR SEARCH BUTTON -->
-      <img id="btn__clear-search" src="../assets/icons/close-black-icon.png" @click="clearSearch()">
-        <div class="search__item" v-for="user in filteredList">
-
-          <p @click="showDetails(user)">
-          
+      <img class="btn__clear-search" src="../assets/icons/close-black-icon.png" @click="clearSearch()">
+      <!-- FILTERED LIST -->
+      <div class="search__item" v-for="user in filteredList">
+        <!-- DISPLAY SHORTENED USER INFO, ON CLICK EXTEND INFO -->
+        <p @click="showDetails(user)">
           <a href="#">
             <!-- display search list thumbnail -->
             <div class="search__list__thumbnail">
               <img :src="user.image">
             </div>
-              <!-- display search list name-->
+            <!-- display search list name-->
             <div class="search__list__name">
               {{ user.fName }} 
               {{ user.lName }}
             </div>
-
             <!-- display desk location -->
             <!-- MAKE FIREBASE REQUEST DEPENDING ON USER-->
             <div class="search__list__desk">
@@ -44,21 +44,27 @@
                 <li>S2</li>
                 <li>A-3</li>
               </ul>
-          </div>
-            </a> 
-          </p>   
-        </div>
+            </div>
+          </a> 
+        </p>   
       </div>
+    </div>
 
-      <!-- NO USER FOUND -->
-      <div class="search__no-user" v-if="isListEmpty">
-        <p>NO USER FOUND. Retry. </p>
-        <img src="https://thumbs.gfycat.com/FeistyLastingKilldeer-size_restricted.gif">
+    <!-- NO USER FOUND -->
+    <div class="search__no-user" v-if="isListEmpty">
+      <p>NO USER FOUND. Retry. </p>
+      <img src="https://thumbs.gfycat.com/FeistyLastingKilldeer-size_restricted.gif">
+    </div>
 
-      </div>
-
-      <!-- SELECTED USER PROFILE -->
-      <app-profile :selectedUser="selectedUser"></app-profile>
+    <!-- SELECTED USER PROFILE -->
+    <app-profile :selectedUser="selectedUser"></app-profile>
+    <!-- CLEAR SEARCH BUTTON -->
+    <img 
+      v-if="Object.keys(selectedUser).length !== 0" 
+      class="btn__clear-search" 
+      id="btn__clear-search-profile" 
+      src="../assets/icons/close-black-icon.png" 
+      @click="clearSearch()">
     </div>
 
 </template>
